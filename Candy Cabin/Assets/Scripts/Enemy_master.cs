358079fabe42858560fab26cbcb70902ba896d6c
jasonzmorton <jasonzmorton@knights.ucf.edu>
2022-02-21 18:35:08 -0500

    Gretel added
    added a basic character that follows when you get close

---
+++ Candy Cabin/Assets/Scripts/Enemy_master.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Enemy_master : MonoBehaviour
+{
+    public GameObject Player;
+    public float Distance;
+
+    public bool isFree;
+
+    public NavMeshAgent _agent;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Distance = Vector3.Distance(Player.transform.position, this.transform.position);
+        
+        if(Distance <= 15f)
+        {
+            isFree = true;
+        }
+        if(Distance > 15f)
+        {
+            isFree = false;
+        }
+
+
+        if(isFree)
+        {
+            _agent.isStopped = false;
+
+            _agent.SetDestination(Player.transform.position);
+        }
+        if(!isFree)
+        {
+            _agent.isStopped = true;
+        }
+    }
+}
