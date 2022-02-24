4f04198a70c85c4abb8b3462b65f77d4b92b4fed
jasonzmorton <jasonzmorton@knights.ucf.edu>
2022-02-24 12:53:38 -0500

    Camera and key update
    Camera is set in the correct direction although it does not stop when it should.
    The key works as a pick up and if you have the key it disables the door on the cage. However gretel is still able to just walk through it

---
+++ Candy Cabin/Assets/Scripts/CameraController.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+
+    public Transform target;
+
+    public Vector3 offset;
+
+    public float pitch = 2f;
+
+    private float currentZoom = 10f;
+
+    void LateUpdate ()
+    {
+        transform.position = target.position - offset * currentZoom;
+        transform.LookAt(target.position + Vector3.up * pitch);
+
+    }
+}
+    // Start is called before the first frame update
+    
