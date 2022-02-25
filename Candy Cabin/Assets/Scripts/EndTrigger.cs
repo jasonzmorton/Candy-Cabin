04976b1a83adbbdaebbaed377442e4262113c2d8
jasonzmorton <jasonzmorton@knights.ucf.edu>
2022-02-25 18:15:04 -0500

    go to menu after victory screen implemented

--- Candy Cabin/Assets/Scripts/EndTrigger.cs
+++ Candy Cabin/Assets/Scripts/EndTrigger.cs
@@ -1,13 +1,22 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class EndTrigger : MonoBehaviour
 {
     public GameManager gameManager;
 
+    
+
     void OnTriggerEnter()
     {
         gameManager.CompleteLevel();
+        Invoke("restartGame", 4);
+    }
+
+    public void restartGame()
+    {
+        SceneManager.LoadScene("Menu");
     }
 }
