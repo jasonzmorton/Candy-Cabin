9f9d97b31388762b9828575e03f4e3e047f33338
jasonzmorton <jasonzmorton@knights.ucf.edu>
2022-02-25 17:51:36 -0500

    added breakable pole in hansel's spawn cell

---
+++ Candy Cabin/Assets/BreakablePole.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BreakablePole : MonoBehaviour
+{
+    public bool inTrigger;
+
+    public GameObject wall;
+ 
+    void OnTriggerEnter(Collider other)
+    {
+        inTrigger = true;
+    }
+ 
+    void OnTriggerExit(Collider other)
+    {
+        inTrigger = false;
+    }
+
+     void Update()
+    {
+        if (inTrigger)
+        {
+            if (Input.GetKeyDown(KeyCode.E))
+            {
+                Destroy(this.gameObject);
+                Destroy(wall);
+            }
+        }
+    }
+
+    void OnGUI()
+    {
+        if (inTrigger)
+        {
+            GUI.Box(new Rect(0, 60, 200, 25), "Press E to break pole");
+        }
+    }
+}
