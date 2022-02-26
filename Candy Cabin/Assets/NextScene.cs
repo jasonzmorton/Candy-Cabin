61c484d3d53e9f810aa75939e240419c34707204
jasonzmorton <jasonzmorton@knights.ucf.edu>
2022-02-26 11:21:41 -0500

    intro scene

---
+++ Candy Cabin/Assets/NextScene.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class NextScene : MonoBehaviour
+{
+    // Start is called before the first frame update
+    
+
+    // Update is called once per frame
+    void Update()
+    {
+        Invoke("nextScene", 7);
+        if (Input.GetKeyDown("space"))
+        {
+            SceneManager.LoadScene("Game");
+        }
+    }
+
+    public void nextScene()
+    {
+        SceneManager.LoadScene("Game");
+    }
+    
+}
